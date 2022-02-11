1dc4cc35adb936307ccd7a722d2a4c7ad1c3f9e2
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-10 19:22:28 -0800

    Refactor: simplify place_piece method in computer class

--- lib/computer.rb
+++ lib/computer.rb
@@ -10,107 +10,23 @@ class Computer
   end
 
   def place_piece(board)
-    columns = ["A", "B", "C", "D", "E", "F", "G"]
-    selection = columns.sample
-    if selection == "A"
-      if board.spaces[-1][0] == "."
-        board.spaces[-1][0] = "O"
-      elsif board.spaces[-2][0] == "."
-        board.spaces[-2][0] = "O"
-      elsif board.spaces[-3][0] == "."
-        board.spaces[-3][0] = "O"
-      elsif board.spaces[-4][0] == "."
-        board.spaces[-4][0] = "O"
-      elsif board.spaces[-5][0] == "."
-        board.spaces[-5][0] = "O"
-      elsif board.spaces[-5][0] == "."
-        board.spaces[-5][0] = "O"
+    selection = board.spaces[0].sample
+
+    valid_space = false
+    row = -1
+    column = board.spaces[0].index(selection)
+
+    until valid_space || row < -6
+      if board.spaces[row][column] == "."
+        board.spaces[row][column] = "O"
+        valid_space = true
       end
-    elsif selection == "B"
-      if board.spaces[-1][1] == "."
-        board.spaces[-1][1] = "O"
-      elsif board.spaces[-2][1] == "."
-        board.spaces[-2][1] = "O"
-      elsif board.spaces[-3][1] == "."
-        board.spaces[-3][1] = "O"
-      elsif board.spaces[-4][1] == "."
-        board.spaces[-4][1] = "O"
-      elsif board.spaces[-5][1] == "."
-        board.spaces[-5][1] = "O"
-      elsif board.spaces[-5][1] == "."
-        board.spaces[-5][1] = "O"
-      end
-    elsif selection == "C"
-      if board.spaces[-1][2] == "."
-        board.spaces[-1][2] = "O"
-      elsif board.spaces[-2][2] == "."
-        board.spaces[-2][2] = "O"
-      elsif board.spaces[-3][2] == "."
-        board.spaces[-3][2] = "O"
-      elsif board.spaces[-4][2] == "."
-        board.spaces[-4][2] = "O"
-      elsif board.spaces[-5][2] == "."
-        board.spaces[-5][2] = "O"
-      elsif board.spaces[-5][2] == "."
-        board.spaces[-5][2] = "O"
-      end
-    elsif selection == "D"
-      if board.spaces[-1][3] == "."
-        board.spaces[-1][3] = "O"
-      elsif board.spaces[-2][3] == "."
-        board.spaces[-2][3] = "O"
-      elsif board.spaces[-3][3] == "."
-        board.spaces[-3][3] = "O"
-      elsif board.spaces[-4][3] == "."
-        board.spaces[-4][3] = "O"
-      elsif board.spaces[-5][3] == "."
-        board.spaces[-5][3] = "O"
-      elsif board.spaces[-5][3] == "."
-        board.spaces[-5][3] = "O"
-      end
-    elsif selection == "E"
-      if board.spaces[-1][4] == "."
-        board.spaces[-1][4] = "O"
-      elsif board.spaces[-2][4] == "."
-        board.spaces[-2][4] = "O"
-      elsif board.spaces[-3][4] == "."
-        board.spaces[-3][4] = "O"
-      elsif board.spaces[-4][4] == "."
-        board.spaces[-4][4] = "O"
-      elsif board.spaces[-5][4] == "."
-        board.spaces[-5][4] = "O"
-      elsif board.spaces[-5][4] == "."
-        board.spaces[-5][4] = "O"
-      end
-    elsif selection == "F"
-      if board.spaces[-1][5] == "."
-        board.spaces[-1][5] = "O"
-      elsif board.spaces[-2][5] == "."
-        board.spaces[-2][5] = "O"
-      elsif board.spaces[-3][5] == "."
-        board.spaces[-3][5] = "O"
-      elsif board.spaces[-4][5] == "."
-        board.spaces[-4][5] = "O"
-      elsif board.spaces[-5][5] == "."
-        board.spaces[-5][5] = "O"
-      elsif board.spaces[-5][5] == "."
-        board.spaces[-5][5] = "O"
-      end
-    elsif selection == "G"
-      if board.spaces[-1][6] == "."
-        board.spaces[-1][6] = "O"
-      elsif board.spaces[-2][6] == "."
-        board.spaces[-2][6] = "O"
-      elsif board.spaces[-3][6] == "."
-        board.spaces[-3][6] = "O"
-      elsif board.spaces[-4][6] == "."
-        board.spaces[-4][6] = "O"
-      elsif board.spaces[-5][6] == "."
-        board.spaces[-5][6] = "O"
-      elsif board.spaces[-5][6] == "."
-        board.spaces[-5][6] = "O"
-      end
+      row -= 1
+    end
+
+    if !valid_space
+      place_piece(board)
     end
   end
-
+  
 end
