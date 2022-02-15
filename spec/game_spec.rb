ecac98f1e285c7917c8a62e873ddc972dea52f62
James Harkins <jh.harkins@protonmail.com>
2022-02-15 16:48:18 -0700

    test: update game_spec.rb for game class refactor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,9 +8,10 @@ require 'pry'
 RSpec.describe Game do
   before(:each) do
     @board = Board.new
-    @player = Player.new
+    @player_one = Player.new
+    @player_two = Player.new
     @computer = Computer.new
-    @game = Game.new(@board, @player, @computer)
+    @game = Game.new(@board, @player_one, @player_two, @computer)
   end
 
   describe '#initialize' do
